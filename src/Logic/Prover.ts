84553322421084a38edb7e7b6effe4a9420c63dd
James Dabbs <jamesdabbs@gmail.com>
2020-11-29 14:45:12 -0800

    Add comment on next goal

--- src/Logic/Prover.ts
+++ src/Logic/Prover.ts
@@ -137,6 +137,20 @@ class Derivations<TheoremId, PropertyId> {
   }
 
   all(): { property: PropertyId, value: boolean, proof: Proof<TheoremId, PropertyId> }[] {
+    // We should now have enough information stored that we can populate this
+    // list directly from `evidence` rather than relying on something external
+    // populating `derivations`.
+    //
+    // Our next goal is to adapt this existing logic from `Prover#run`:
+    //
+    //   this.traits.forEach((value: boolean, property: PropertyId) => {
+    //     const proof = this.derivations.proof(property)
+    //     if (!proof || proof === 'given') {
+    //       return
+    //     }
+
+    //     this.derivations.add({ property, value, proof })
+    //   })
     return [...this.derivations.entries()].map(
       ([property, [value, proof]]) => ({ property, value, proof })
     )
